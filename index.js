010ffa3429385e9f326d27d92ab10a0a172161bc
Rakesh Patil <irakeshpatil@gmail.com>
2020-08-23 20:40:22 +0530

    isthisrevert?

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const path = require('path')
 var enforce = require('express-sslify')
 const PORT = process.env.PORT || 5000
 
-express()
-.use(enforce.HTTPS())
-  .use(express.static(path.join(__dirname, 'public')))
+var app = express();
+
+
+app.use(enforce.HTTPS());
+
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
