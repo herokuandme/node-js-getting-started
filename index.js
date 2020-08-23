49a098e3adcb2950e44db6a94af979f71902cf2d
Rakesh Patil <irakeshpatil@gmail.com>
2020-08-23 19:58:36 +0530

    forcessl

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+var enforce = require('express-sslify')
 const PORT = process.env.PORT || 5000
 
 express()
+.use(enforce.HTTPS())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
